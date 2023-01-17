a106f14967596d3384868316bd3247a5c684402b
yonahara127 <107020969+yonahara127@users.noreply.github.com>
2023-01-17 15:01:46 +0900

    画像追加

--- PG_gamekadai/program_file/SELECT.h
+++ PG_gamekadai/program_file/SELECT.h
@@ -2,9 +2,9 @@
 #include"AbstractScene.h"
 
 #define LETT_X 570
-#define LETT_Y 80
+#define LETT_Y 170
 #define LETT_SPACE 160
-#define CURSOL_SIZE_Y 80
+#define CURSOL_SIZE_Y 170
 #define CURSOL_SIZE_X 
 
 enum class DIFFICULTY
@@ -18,11 +18,15 @@ class SELECT : public AbstractScene
 {
 private:
 	int cursol_x, cursol_y;
+	int cursol;
+	int selectimage;
 	DIFFICULTY difficul;
 public:
 
 	SELECT();
-	~SELECT() {};
+	~SELECT() {
+		
+	};
 
 	void Draw() const override;
 	AbstractScene* Update() override;
