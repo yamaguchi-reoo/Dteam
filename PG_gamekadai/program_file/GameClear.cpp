6d9d9bd7ffc7a6cd4d57d7fc07237cda47988fec
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2023-01-17 10:46:50 +0900

    a

--- PG_gamekadai/program_file/GameClear.cpp
+++ PG_gamekadai/program_file/GameClear.cpp
@@ -1,18 +1,19 @@
-//#include "GameClear.h"
-//#include"DxLib.h"
-//#include<stdio.h>
-//
-//GameClear::GameClear()
-//{
-//
-//}
-//
-//void GameClear::Draw() const
-//{
-//
-//}
-//
-//AbstractScene* GameClear::Update()
-//{
-//	return this;
-//}
+#include "GameClear.h"
+#include"DxLib.h"
+#include<stdio.h>
+
+GameClear::GameClear()
+{
+
+}
+
+void GameClear::Draw() const
+{
+	SetFontSize(16);
+	DrawString(150, 420, " ---- Bボタンでメニュー画面へ ----", 0xffffff, 0x000000);
+}
+
+AbstractScene* GameClear::Update()
+{
+	return this;
+}
