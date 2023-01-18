7a10abf763ea28feb1ed510dee6a2a0906840d48
noname604 <122337128+noname604@users.noreply.github.com>
2023-01-18 12:02:22 +0900

    仮完成

--- PG_gamekadai/program_file/GameEnd.cpp
+++ PG_gamekadai/program_file/GameEnd.cpp
@@ -25,21 +25,21 @@ AbstractScene* GameEnd::Update()
 
 void GameEnd::Draw() const
 {
-	DrawGraph(0, 0, EndImg, TRUE);
+	//DrawGraph(0, 0, EndImg, TRUE);
 
 	SetFontSize(24);
-	DrawString(450, 170 + endy, "        タイトル　　　", 0x000000);
-	DrawString(450, 200 + endy, "　　      「」         ", 0x000000);
-	DrawString(450, 230 + endy, "         制作者    ", 0x000000);
-	DrawString(450, 260 + endy, "☆2-4　○○　　○○", 0x000000);
-	DrawString(450, 290 + endy, "　2-4　○○　　○○", 0x000000);
-	DrawString(450, 320 + endy, "　2-3　○○　　○○", 0x000000);
-	DrawString(450, 350 + endy, "　1-4　山口　　麗王", 0x000000);
-	DrawString(450, 380 + endy, "　1-3　比屋根　綾寄", 0x000000);
-	DrawString(450, 410 + endy, "　1-3　宮城　　直希", 0x000000);
+	DrawString(450, 170 + endy, "        タイトル   ", 0xFFFFFF);
+	DrawString(450, 200 + endy, "　　  「いろあい」 ", 0xFFFFFF);
+	DrawString(450, 230 + endy, "         制作者    ", 0xFFFFFF);
+	DrawString(450, 260 + endy, "☆2-4　崎間　　竜也", 0xFFFFFF);
+	DrawString(450, 290 + endy, "　2-4　佐久本　盛杏", 0xFFFFFF);
+	DrawString(450, 320 + endy, "　2-3　與那原　廉  ", 0xFFFFFF);
+	DrawString(450, 350 + endy, "　1-4　山口　　麗王", 0xFFFFFF);
+	DrawString(450, 380 + endy, "　1-3　比屋根　綾寄", 0xFFFFFF);
+	DrawString(450, 410 + endy, "　1-3　宮城　　直希", 0xFFFFFF);
 	DrawString(450, 440 + endy, "素材利用", 0xFFFFFF);
 	DrawString(450, 480 + endy, "　BGM　　　 　ＸＸＸＸ", 0xFFFFFF);
 	DrawString(450, 495 + endy, "　SE　　　　　ＸＸＸＸＸＸＸＸ", 0xFFFFFF);
-	DrawString(450, 600 + endy, "Thank you for playing!!", 0xFFFFFF);
+	DrawString(450, 600 + endy, " Thank you for playing!!", 0xFFFFFF);
 
 }
