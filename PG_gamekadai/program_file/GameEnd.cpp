1228127039565c8819320afd890cc4e7bb6e5357
noname604 <122337128+noname604@users.noreply.github.com>
2023-01-18 09:56:23 +0900

    エンドロールの文字追加

--- PG_gamekadai/program_file/GameEnd.cpp
+++ PG_gamekadai/program_file/GameEnd.cpp
@@ -17,7 +17,7 @@ AbstractScene* GameEnd::Update()
 	if (++endtime < 600) { endy = 300 - endtime / 2; }
 
 	//タイムの加算処理＆終了
-	if (endtime > 900){ exit(1); }
+	if (endtime > 900){ exit(0); }
 
 	return this;
 }
@@ -29,16 +29,18 @@ void GameEnd::Draw() const
 
 	SetFontSize(24);
 	DrawString(450, 170 + endy, "        タイトル　　　", 0xFFFFFF);
-	DrawString(450, 200 + endy, "　　      「」          ", 0xFFFFFF);
-	DrawString(450, 230 + endy, "         制作者     ", 0xFFFFFF);
-	DrawString(450, 260 + endy, "  2-    　　　　", 0xFFFFFF);
-	DrawString(450, 290 + endy, "　2-　 Naoki　Miyagi", 0xFFFFFF);
-	DrawString(450, 310 + endy, "　2-　 Naoki　Miyagi", 0xFFFFFF);
-	DrawString(450, 340 + endy, "　1-4　山口　  麗王", 0xFFFFFF);
-	DrawString(450, 370 + endy, "　1-3　比屋根　綾寄", 0xFFFFFF);
-	DrawString(450, 400 + endy, "　1-3　宮城　　直希", 0xFFFFFF);
-	DrawString(450, 430 + endy, "素材利用", 0xFFFFFF);
-	DrawString(450, 470 + endy, "　BGM　　　 　ＸＸＸＸ", 0xFFFFFF);
-	DrawString(450, 485 + endy, "　SE　　　　　ＸＸＸＸＸＸＸＸ", 0xFFFFFF);
+	DrawString(450, 200 + endy, "　　      「」         ", 0xFFFFFF);
+	DrawString(450, 230 + endy, "         制作者    ", 0xFFFFFF);
+	DrawString(450, 260 + endy, "☆2-4　○○　　○○", 0xFFFFFF);
+	DrawString(450, 290 + endy, "　2-4　○○　　○○", 0xFFFFFF);
+	DrawString(450, 320 + endy, "　2-3　○○　　○○", 0xFFFFFF);
+	DrawString(450, 350 + endy, "　1-4　山口　　麗王", 0xFFFFFF);
+	DrawString(450, 380 + endy, "　1-3　比屋根　綾寄", 0xFFFFFF);
+	DrawString(450, 410 + endy, "　1-3　宮城　　直希", 0xFFFFFF);
+	DrawString(450, 440 + endy, "素材利用", 0xFFFFFF);
+	DrawString(450, 480 + endy, "　BGM　　　 　ＸＸＸＸ", 0xFFFFFF);
+	DrawString(450, 495 + endy, "　SE　　　　　ＸＸＸＸＸＸＸＸ", 0xFFFFFF);
+	DrawString(450, 600 + endy, "Thank you for playing!!", 0xFFFFFF);
+
 
 }
